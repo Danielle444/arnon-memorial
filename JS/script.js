b6d4c50ac46c013ce58dadea19167bf846dc642b
Danielle Kaneti <Kanetidandi@gmail.com>
2025-06-26 19:21:37 +0300

    סידור גלילה

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,11 @@ function toggleMenu() {
   const nav = document.getElementById('main-nav');
   nav.classList.toggle('active');
 }
+function scrollCarousel(direction) {
+  var track = document.getElementById('carouselTrack');
+  var scrollAmount = track.offsetWidth * 0.6; // גולש 60% מהרוחב
+  track.scrollBy({
+    left: direction * scrollAmount,
+    behavior: 'smooth'
+  });
+}
